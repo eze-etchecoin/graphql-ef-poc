1a35080ad52c917ebb91cc405587c0df5acd2a78
Ezequiel Etchecoin <ezequiel.etchecoin@incluit.com>
2023-10-23 23:59:26 -0300

    added DbContext and connection string

---
+++ GraphQL-EF-PoC/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GraphQL_EF_PoC.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }  
+    }
+}
--- GraphQL-EF-PoC/Program.cs
+++ GraphQL-EF-PoC/Program.cs
@@ -1,4 +1,13 @@
+using GraphQL_EF_PoC.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
