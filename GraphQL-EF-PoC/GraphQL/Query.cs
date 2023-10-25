558da5d1b5f0d5a09b08876c6b3c849889ab3617
Ezequiel Etchecoin <ezequiel.etchecoin@incluit.com>
2023-10-24 23:51:47 -0300

    created graphql query

---
+++ GraphQL-EF-PoC/GraphQL/Query.cs
@@ -0,0 +1,12 @@
+﻿using GraphQL_EF_PoC.Data;
+
+namespace GraphQL_EF_PoC.GraphQL
+{
+    public class Query
+    {
+        public IQueryable<Vehicle> GetVehicle([Service]AppDbContext context)
+        {
+            return context.Vehicles;
+        }
+    }
+}
--- GraphQL-EF-PoC/Program.cs
+++ GraphQL-EF-PoC/Program.cs
@@ -1,4 +1,5 @@
 using GraphQL_EF_PoC.Data;
+using GraphQL_EF_PoC.GraphQL;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,8 +9,15 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>();
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGraphQL();
 
 app.Run();
